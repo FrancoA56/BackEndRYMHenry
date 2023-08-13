6597d223c78cee49dd5d43b393f220538bc737e3
Franco Adamoli <franco.adamoli@gmail.com>
2023-08-13 01:25:31 -0300

    Finalizado

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,16 +1,11 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const UserModel = require("./models/User.js");
 const FavoritesModel = require("./models/Favorite.js");
 
-// const database = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
-//   { logging: false, native: false }
-// );
-
 const database = new Sequelize(
-  DB_DEPLOY,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
   { logging: false, native: false }
 );
 
